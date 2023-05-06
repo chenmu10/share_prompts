aaabb525cb30ac8aa0d3ccc897cde2433131066e
Chen Mualem <chenmu10@gmail.com>
2023-05-06 23:48:00 +0300

    basic structure and config

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Feed from '@components/Feed.jsx';
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+
+    <h1 className="head_text text-center">
+      Discover & Share
+      <br className="max-md:hidden" />
+      <span className="orange_gradient"> AI Powered Prompts</span>
+    </h1>
+    <p className="desc text-center">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium illo harum corrupti, earum id commodi ullam dolorum. Culpa accusamus inventore, esse quis veritatis repellendus porro quidem rem corporis similique dolores?
+    </p>
+    <Feed/>
+    </section>
+  )
+}
+
+export default Home
---
+++ components/Nav.jsx
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import {signIn,signOut,useSession,getProviders} from 'next-auth/react'
+
+
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
