ddc0cd6281b272f88fd70354714e03ee8e628da1
Chen Mualem <chenmu10@gmail.com>
2023-05-11 13:55:50 +0300

    [feat] adding new prompt

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,67 @@
+import Link from "next/link";
 
-const From = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>From</div>
-  )
-}
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Laborum, eos, unde ea suscipit excepturi libero tenetur accusantium
+        sint, eaque aperiam dolores maiores autem doloribus sed. Maiores animi
+        eveniet veritatis velit?
+      </p>
+      <form
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+        onSubmit={handleSubmit}
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            className="form_textarea"
+            onChange={(e) => {
+              setPost({ ...post, prompt: e.target.value });
+            }}
+            placeholder="Write your prompt here..."
+            required
+            value={post.prompt}
+          ></textarea>
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag{" "}
+            <span className="font-normal">
+              (#product, #webdevelopment, #idea)
+            </span>
+          </span>
+          <input
+            className="form_input"
+            onChange={(e) => {
+              setPost({ ...post, tag: e.target.value });
+            }}
+            placeholder="#tag"
+            required
+            value={post.tag}
+          ></input>
+        </label>
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link className="text-gray-500 text-sm" href="/">
+            Cancel
+          </Link>
+          <button
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+            disabled={submitting}
+            type="submit"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
 
-export default From
+export default Form;
